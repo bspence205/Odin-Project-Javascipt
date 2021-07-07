69adf023d382814a096792d8daef083e13ee548f
bspence205 <77554087+bspence205@users.noreply.github.com>
2021-07-07 01:10:20 -0700

    practicing fundamentals

--- script.js
+++ script.js
@@ -13,3 +13,56 @@ let ourPlanet = "Earth";
 let currentUserName = "John";
 
 //--------Numbers--------//
+
+let lotsOfDecimal = 1.766584958675746364;
+lotsOfDecimal;
+let twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+twoDecimalPlaces;
+console.log(twoDecimalPlaces);
+
+let num1 = 10;
+let num2 = 50;
+9 * num1;
+num1 ** 3;
+num2 / num1;
+
+5 + 10 * 3;
+(num2 % 9) * num1;
+let x = num2 + num1 / 8 + 2;
+console.log(x);
+
+let myNum = 2;
+myNum++;
+console.log(myNum);
+
+let mySub = 4;
+mySub--;
+console.log(mySub);
+
+let myNuN = 2;
+++myNuN;
+console.log(myNuN);
+
+let mySuN = 4;
+--mySuN;
+console.log(mySuN);
+
+// Fundamentals pt. 1 Practice: https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/fundamentals-part-1 //
+
+console.log(28 + 6);
+console.log(1 + 4 + 5 + 1 + 5 + 2);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+console.log(9 * a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
+
+// Strings //
